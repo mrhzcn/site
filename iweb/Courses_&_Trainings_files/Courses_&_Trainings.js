71d751effbaf1b170438e383771f6ae03f635fbd
Emrah Özcan <ozcanemrah@gmail.com>
2019-06-16 08:25:40 +0300

    delete blog feature

--- iweb/Courses_&_Trainings_files/Courses_&_Trainings.js
+++
@@ -1,9 +0,0 @@
-// Created by iWeb 3.0.4 local-build-20190614
-
-setTransparentGifURL('Media/transparent.gif');function hostedOnDM()
-{return false;}
-function onPageLoad()
-{loadMozillaCSS('Courses_&_Trainings_files/Courses_&_TrainingsMoz.css')
-adjustLineHeightIfTooBig('id1');adjustFontSizeIfTooBig('id1');adjustLineHeightIfTooBig('id2');adjustFontSizeIfTooBig('id2');adjustLineHeightIfTooBig('id3');adjustFontSizeIfTooBig('id3');Widget.onload();fixAllIEPNGs('Media/transparent.gif');performPostEffectsFixups()}
-function onPageUnload()
-{Widget.onunload();}
